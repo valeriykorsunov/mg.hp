1775d0eebabef73c2b3df8d15c48eff5f2bd9489
Valeriy <valeriy@magwai.ru>
2023-01-10 17:07:07 +0300

    fix

--- lib/Main/EventHandler.php
+++ lib/Main/EventHandler.php
@@ -8,7 +8,6 @@ class EventHandler {
 	}
 
 	public static function OnEpilog(){
-		\Bitrix\Main\Diag\Debug::dumpToFile("test 2.0" ,'*'.date('Y-m-d H:i:s').'*'. PHP_EOL .__FILE__);
 		\MG\HP\Debug::showLog();
 	}
 }
