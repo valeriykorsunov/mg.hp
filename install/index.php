cfbfe6c6ccbbfc4472f967057be981933555937d
Nina Chaplina <nina@magwai.ru>
2023-02-08 12:02:37 +0300

    Новый тип для пользовательского свойства Текст HTML, добавляется сразу после установки модуля

--- install/index.php
+++ install/index.php
@@ -2,6 +2,9 @@
 
 use Bitrix\Main\Loader;
 use Bitrix\Main\Application;
+use Bitrix\Main\Event;
+use Bitrix\Main\EventManager;
+use Bitrix\Main\EventResult;
 use Bitrix\Main\ModuleManager;
 use Bitrix\Main\Entity\Base;
 use Bitrix\Main\Localization\Loc;
@@ -10,217 +13,225 @@ Loc::loadMessages(__FILE__);
 class mg_hp extends CModule
 {
 
-	function __construct()
-	{
-		$arModuleVersion = array();
-		include(__DIR__ . '/version.php');
-
-		$this->MODULE_ID = 'mg.hp';
-		$this->MODULE_VERSION = $arModuleVersion["VERSION"];
-		$this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
-		$this->MODULE_NAME = "HELPER";
-		$this->MODULE_DESCRIPTION = "HELPER"; 
-
-		$this->PARTNER_NAME = "MG";
-		$this->PARTNER_URI = "http://bxwork.ru";
-
-		$this->MODULE_SORT = 1;
-		$this->SHOW_SUPER_ADMIN_GROUP_RIGHTS = 'Y';
-		$this->MODULE_GROUP_RIGHTS = 'Y';  //используем ли индивидуальную схему распределения прав доступа
-
-		$this->NAME_DIRECTORY = substr(strrchr(dirname(__DIR__, 3), "/"), 1); // local или bitrix
-	}
-
-	function DoInstall()
-	{
-		global $APPLICATION;
-		if ($this->isVersionD7() && $this->isVersionPhp())
-		{
-			ModuleManager::registerModule($this->MODULE_ID);
-
-			// RegisterModuleDependences("main", "OnUserTypeBuildList", "mg.hp", "СComplexUserProperty", "getDescription");
-
-			Loader::includeModule($this->MODULE_ID);
-			$this->InstallDB();
-			$this->editHandler("install");
-			$this->editFiles("install");
-		}
-		else
-		{
-			$APPLICATION->ThrowException('Версия ядра Битрикс не поддерживается или версия php ниже необходимой.');
-		}
-
-		$APPLICATION->IncludeAdminFile("INSTALL TITLE", $this->GetPath() . "/install/step.php");
-	}
-
-	function DoUninstall()
-	{
-		if (!check_bitrix_sessid()) return false;
-		global $APPLICATION;
-		$context = Application::getInstance()->getContext();
-		$request = $context->getRequest();
-		if ($request["step"] < 2)
-		{
-			$APPLICATION->IncludeAdminFile("UNINSTALL TITLE", $this->GetPath() . "/install/unstep1.php");
-		}
-		elseif ($request["step"] == 2)
-		{
-			if ($request["savedata"] != "Y")
-			{
-				$this->UnInstallDB();
-			}
-	
-			$this->editHandler("uninstall");
-			$this->editFiles("uninstall");
-
-			// UnRegisterModuleDependences("main", "OnUserTypeBuildList", "mg.hp", "СComplexUserProperty", "getDescription");
-	
-			ModuleManager::unRegisterModule($this->MODULE_ID);
-			$APPLICATION->IncludeAdminFile("UNINSTALL TITLE", $this->GetPath() . "/install/unstep2.php");
-		}
-	}
-
-	function InstallDB()
+    function __construct()
     {
-		Loader::includeModule($this->MODULE_ID);
+        $arModuleVersion = array();
+        include(__DIR__ . '/version.php');
 
-		if(!Application::getConnection(\MG\HP\Main\TabsTable::getConnectionName())->isTableExists(Base::getInstance('\MG\HP\Main\TabsTable')->getDBTableName()))
+        $this->MODULE_ID = 'mg.hp';
+        $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+        $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+        $this->MODULE_NAME = "HELPER";
+        $this->MODULE_DESCRIPTION = "HELPER";
+
+        $this->PARTNER_NAME = "MG";
+        $this->PARTNER_URI = "http://bxwork.ru";
+
+        $this->MODULE_SORT = 1;
+        $this->SHOW_SUPER_ADMIN_GROUP_RIGHTS = 'Y';
+        $this->MODULE_GROUP_RIGHTS = 'Y';  //используем ли индивидуальную схему распределения прав доступа
+
+        $this->NAME_DIRECTORY = substr(strrchr(dirname(__DIR__, 3), "/"), 1); // local или bitrix
+    }
+
+    function DoInstall()
+    {
+        global $APPLICATION;
+        if ($this->isVersionD7() && $this->isVersionPhp())
+        {
+            $eventManager = \Bitrix\Main\EventManager::getInstance();
+            $eventManager->registerEventHandler('main', 'OnUserTypeBuildList', 'mg.hp', 'CCustomTypeHtml', 'GetUserTypeDescription');
+            unset($eventManager);
+            ModuleManager::registerModule($this->MODULE_ID);
+
+            // RegisterModuleDependences("main", "OnUserTypeBuildList", "mg.hp", "СComplexUserProperty", "getDescription");
+
+            Loader::includeModule($this->MODULE_ID);
+            $this->InstallDB();
+            $this->editHandler("install");
+            $this->editFiles("install");
+        }
+        else
+        {
+            $APPLICATION->ThrowException('Версия ядра Битрикс не поддерживается или версия php ниже необходимой.');
+        }
+
+        $APPLICATION->IncludeAdminFile("INSTALL TITLE", $this->GetPath() . "/install/step.php");
+    }
+
+    function DoUninstall()
+    {
+        if (!check_bitrix_sessid()) return false;
+        global $APPLICATION;
+        $context = Application::getInstance()->getContext();
+        $request = $context->getRequest();
+        if ($request["step"] < 2)
+        {
+            $APPLICATION->IncludeAdminFile("UNINSTALL TITLE", $this->GetPath() . "/install/unstep1.php");
+        }
+        elseif ($request["step"] == 2)
+        {
+            if ($request["savedata"] != "Y")
+            {
+                $this->UnInstallDB();
+            }
+
+            $this->editHandler("uninstall");
+            $this->editFiles("uninstall");
+
+
+            // UnRegisterModuleDependences("main", "OnUserTypeBuildList", "mg.hp", "СComplexUserProperty", "getDescription");
+            $eventManager = \Bitrix\Main\EventManager::getInstance();
+            $eventManager->unRegisterEventHandler('main', 'OnUserTypeBuildList', 'mg.hp', 'CCustomTypeHtml', 'GetUserTypeDescription');
+            unset($eventManager);
+            ModuleManager::unRegisterModule($this->MODULE_ID);
+            $APPLICATION->IncludeAdminFile("UNINSTALL TITLE", $this->GetPath() . "/install/unstep2.php");
+        }
+    }
+
+    function InstallDB()
+    {
+        Loader::includeModule($this->MODULE_ID);
+
+        if(!Application::getConnection(\MG\HP\Main\TabsTable::getConnectionName())->isTableExists(Base::getInstance('\MG\HP\Main\TabsTable')->getDBTableName()))
         {
             Base::getInstance('\MG\HP\Main\TabsTable')->createDbTable();
         }
-		
+
         if(!Application::getConnection(\MG\HP\Main\TabsUserFieldUsTable::getConnectionName())->isTableExists(Base::getInstance('\MG\HP\Main\TabsUserFieldUsTable')->getDBTableName()))
         {
             Base::getInstance('\MG\HP\Main\TabsUserFieldUsTable')->createDbTable();
         }
-	}
-	
-	function UnInstallDB()
-	{
+
+    }
+
+    function UnInstallDB()
+    {
         Loader::includeModule($this->MODULE_ID);
 
         Application::getConnection(\MG\HP\Main\TabsTable::getConnectionName())->queryExecute('drop table if exists '.Base::getInstance('\MG\HP\Main\TabsTable')->getDBTableName());
 
         Application::getConnection(\MG\HP\Main\TabsUserFieldUsTable::getConnectionName())->queryExecute('drop table if exists '.Base::getInstance('\MG\HP\Main\TabsUserFieldUsTable')->getDBTableName());
-	}
 
-	protected function isVersionD7()
-	{
-		return CheckVersion(\Bitrix\Main\ModuleManager::getVersion("main"), "20.00.00"); 
-	}
+    }
 
-	protected function isVersionPhp($version = '7.4')
-	{
-		return (phpversion() * 10 >= $version * 10);
-	}
+    protected function isVersionD7()
+    {
+        return CheckVersion(\Bitrix\Main\ModuleManager::getVersion("main"), "20.00.00");
+    }
 
-	protected function GetPath($notDocumentRoot = false)
-	{
-		if ($notDocumentRoot)
-			return str_ireplace(Application::getDocumentRoot(), '', dirname(__DIR__));
-		else
-			return dirname(__DIR__);
-	}
+    protected function isVersionPhp($version = '7.4')
+    {
+        return (phpversion() * 10 >= $version * 10);
+    }
 
-	/**
-	 * Обработчик событий
-	 *
-	 * @param [string] $typeAction
-	 * @return bool
-	 */
-	protected function editHandler(string $typeAction)
-	{
-		$listHendler = array(
-			["ModuleId" => "main", "Event" => "onPageStart", "Sort" => "100"],
-			["ModuleId" => "main", "Event" => "OnEpilog", "Sort" => "100"],
-		);
+    protected function GetPath($notDocumentRoot = false)
+    {
+        if ($notDocumentRoot)
+            return str_ireplace(Application::getDocumentRoot(), '', dirname(__DIR__));
+        else
+            return dirname(__DIR__);
+    }
 
-		foreach ($listHendler as $params)
-		{
-			if ($typeAction == "install")
-			{
-				$this->registerHandler($params);
-			}
-			if ($typeAction == "uninstall")
-			{
-				$this->unregisterHandler($params);
-			}
-		}
-		return	true;
-	}
-	
-	protected function registerHandler(array $params)
-	{
-		if (!isset($params["ModuleId"], $params["Event"], $params["Sort"])) return false; // TODO зафиксировать как ошибку.
+    /**
+     * Обработчик событий
+     *
+     * @param [string] $typeAction
+     * @return bool
+     */
+    protected function editHandler(string $typeAction)
+    {
+        $listHendler = array(
+            ["ModuleId" => "main", "Event" => "onPageStart", "Sort" => "100"],
+            ["ModuleId" => "main", "Event" => "OnEpilog", "Sort" => "100"],
+        );
 
-		\Bitrix\Main\EventManager::getInstance()->registerEventHandler(
-			$params["ModuleId"],
-			$params["Event"],
-			$this->MODULE_ID,
-			'MG\HP\Main\EventHandler',
-			$params["Event"],
-			$params["Sort"]
-		);
-		return true;
-	}
-	protected function unregisterHandler(array $params)
-	{
-		if (!isset($params["ModuleId"], $params["Event"], $params["Sort"])) return false; // TODO зафиксировать как ошибку.
+        foreach ($listHendler as $params)
+        {
+            if ($typeAction == "install")
+            {
+                $this->registerHandler($params);
+            }
+            if ($typeAction == "uninstall")
+            {
+                $this->unregisterHandler($params);
+            }
+        }
+        return	true;
+    }
 
-		\Bitrix\Main\EventManager::getInstance()->unregisterEventHandler(
-			$params["ModuleId"],
-			$params["Event"],
-			$this->MODULE_ID,
-			'MG\HP\Main\EventHandler',
-			$params["Event"]
-		);
-		return true;
-	}
+    protected function registerHandler(array $params)
+    {
+        if (!isset($params["ModuleId"], $params["Event"], $params["Sort"])) return false; // TODO зафиксировать как ошибку.
 
-	/**
-	 * Обработчик файлов
-	 *
-	 * @param string $typeAction
-	 * @return void
-	 */
-	protected function editFiles(string $typeAction)
-	{
-		CopyDirFiles($_SERVER["DOCUMENT_ROOT"]."/".$this->MY_DIR."/modules/".$this->MODULE_ID."/install/themes/", $_SERVER["DOCUMENT_ROOT"]."/bitrix/themes/", true, true);
+        \Bitrix\Main\EventManager::getInstance()->registerEventHandler(
+            $params["ModuleId"],
+            $params["Event"],
+            $this->MODULE_ID,
+            'MG\HP\Main\EventHandler',
+            $params["Event"],
+            $params["Sort"]
+        );
+        return true;
+    }
+    protected function unregisterHandler(array $params)
+    {
+        if (!isset($params["ModuleId"], $params["Event"], $params["Sort"])) return false; // TODO зафиксировать как ошибку.
 
-		$arrayFilePath = [
-			["from" => $this->GetPath() . "/install/admin/", "to" => $_SERVER["DOCUMENT_ROOT"] . "/bitrix/admin", "recursive" => false],
-			["from" => $this->GetPath() . "/install/themes/", "to" => $_SERVER["DOCUMENT_ROOT"] . "/bitrix/themes", "recursive" => true]
-		];
+        \Bitrix\Main\EventManager::getInstance()->unregisterEventHandler(
+            $params["ModuleId"],
+            $params["Event"],
+            $this->MODULE_ID,
+            'MG\HP\Main\EventHandler',
+            $params["Event"]
+        );
+        return true;
+    }
 
-		foreach ($arrayFilePath as $arPath)
-		{
-			if ($typeAction == "install")
-			{
-				$this->filesInstall($arPath["from"], $arPath["to"], $arPath["recursive"]);
-			}
-			if ($typeAction == "uninstall")
-			{
-				$this->filesUninstall($arPath["from"], $arPath["to"], $arPath["recursive"]);
-			}
-		}
-		return true;
-	}
+    /**
+     * Обработчик файлов
+     *
+     * @param string $typeAction
+     * @return void
+     */
+    protected function editFiles(string $typeAction)
+    {
+        CopyDirFiles($_SERVER["DOCUMENT_ROOT"]."/".$this->MY_DIR."/modules/".$this->MODULE_ID."/install/themes/", $_SERVER["DOCUMENT_ROOT"]."/bitrix/themes/", true, true);
 
-	protected function filesInstall(string $path_from, string $path_to, bool $recursive = false)
-	{
-		CopyDirFiles($path_from, $path_to, true, $recursive);
-		return true;
-	}
-	
-	protected function filesUninstall(string $path_from = "", string $path_to, bool $recursive = false)
-	{
-		if ($recursive)
-		{
-			DeleteDirFilesEx($path_to);
-			return true;
-		}
+        $arrayFilePath = [
+            ["from" => $this->GetPath() . "/install/admin/", "to" => $_SERVER["DOCUMENT_ROOT"] . "/bitrix/admin", "recursive" => false],
+            ["from" => $this->GetPath() . "/install/themes/", "to" => $_SERVER["DOCUMENT_ROOT"] . "/bitrix/themes", "recursive" => true]
+        ];
 
-		DeleteDirFiles($path_from, $path_to);
-		return true;
-	}
+        foreach ($arrayFilePath as $arPath)
+        {
+            if ($typeAction == "install")
+            {
+                $this->filesInstall($arPath["from"], $arPath["to"], $arPath["recursive"]);
+            }
+            if ($typeAction == "uninstall")
+            {
+                $this->filesUninstall($arPath["from"], $arPath["to"], $arPath["recursive"]);
+            }
+        }
+        return true;
+    }
+
+    protected function filesInstall(string $path_from, string $path_to, bool $recursive = false)
+    {
+        CopyDirFiles($path_from, $path_to, true, $recursive);
+        return true;
+    }
+
+    protected function filesUninstall(string $path_from = "", string $path_to, bool $recursive = false)
+    {
+        if ($recursive)
+        {
+            DeleteDirFilesEx($path_to);
+            return true;
+        }
+
+        DeleteDirFiles($path_from, $path_to);
+        return true;
+    }
 }
