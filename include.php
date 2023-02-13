cf558e0f8fd9353860f76b043d74c165c2f97beb
NikaChunaeva <115978039+NikaChunaeva@users.noreply.github.com>
2023-02-13 10:29:31 +0300

    Update include.php

--- include.php
+++ include.php
@@ -3,7 +3,7 @@
 \Bitrix\Main\Loader::registerAutoLoadClasses(
     "mg.hp",
     array(
-        // 'Bogdo\Debug' => 'classes/general/Debug.php',
+        // 'Bogdo\Debug'   => 'classes/general/Debug.php',
         'MgHpSettings' => 'classes/general/MgHpSettings.php',
         // 'CCustomTypeHtml' => 'classes/general/ccustomtypehtml.php'
     )
