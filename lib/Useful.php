8b38294b9be34f6094e5315308c99e4885c9404b
Valeriy Korsunov <valeriy@magwai.ru>
2023-07-25 08:55:16 +0300

    +

---
+++ lib/Useful.php
@@ -0,0 +1,47 @@
+<?
+namespace MG\HP;
+
+/**
+ * Полезные функции
+ */
+class Useful
+{
+	/**
+	 * Приводит к нормальному виду массив меню
+	 * 
+	 * @param array $nav Bitrix menu nav array
+	 * @return array normalized menu
+	 */
+	public static function normalizeMenuNav(array $nav): array
+	{
+		foreach ($nav as $key => $arItem) {
+			if ($arItem['DEPTH_LEVEL'] > 1) {
+				for ($i = $key - 1; $i >= 0; $i--) {
+					if ($nav[$i]['DEPTH_LEVEL'] < $arItem['DEPTH_LEVEL']) {
+						$nav[$i]['CHILDREN'][] = $key;
+						break;
+					}
+				}
+			}
+		}
+
+		$children = function (&$item, &$list) use (&$children) {
+			if (!empty($item['CHILDREN'])) {
+				foreach ($item['CHILDREN'] as $key => $id) {
+					$childItem = $list[$id];
+					if (!empty($childItem['CHILDREN'])) {
+						$children($childItem, $list);
+					}
+					$item['CHILDREN'][$key] = $childItem;
+					unset($list[$id]);
+				}
+			}
+		};
+
+		foreach ($nav as $i => $arItem) {
+			$children($nav[$i], $nav);
+		}
+		$nav = array_filter($nav);
+		return array_values($nav);
+	}
+}
